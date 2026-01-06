48902d979fa1350bbcd780d99bd42520d681762d
Hiba <Hiba@Hibas-MacBook-Pro.local>
2026-01-06 14:12:56 +0100

    Bloc 4 : alarme (tuple) fonctionnelle

--- clock.py
+++ clock.py
@@ -4,14 +4,26 @@ def afficher_heure(heure):
         h, m, s = heure
         print(f"{h:02d}:{m:02d}:{s:02d}")
 
+def regler_alarme():
+        h = int(input("Heures alarme : "))
+        m = int(input("Minutes alarme : "))
+        s = int(input("Secondes alarme : "))
+        return (h, m, s)
+
 h = int(input("Heures (0-23) : "))
 m = int(input("Minutes (0-59) : "))
 s = int(input("Secondes (0-59) : "))
 
 heure = (h, m, s)
+alarme = regler_alarme()
 
 while True:
         afficher_heure(heure)
+
+        if alarme is not None and heure == alarme:
+            print("⏰ ALARME !")
+            alarme = None
+
         time.sleep(1)
 
         h, m, s = heure
@@ -26,5 +38,15 @@ while True:
         if h == 24:
                 h = 0
         heure = (h, m, s)
+
+       
+
+        
+
+
+
+
+
+
 
 
