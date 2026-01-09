13e9855e8ad365a60389fd6053d6fe931c8ad29d
GA2207 <guillaume.averbouch@laplateforme.io>
2026-01-09 10:34:49 +0100

    Version final

--- clock_pygame.py
+++ clock_pygame.py
@@ -152,7 +152,7 @@ class TextBox:
 # Main
 def main():
     pygame.init()
-    pygame.display.set_caption("Horloge Moderne (noir/or) - complète")
+    pygame.display.set_caption("Horloge de Mamie Jeannine")
 
     W, H = 1100, 720
     screen = pygame.display.set_mode((W, H))
@@ -401,7 +401,7 @@ def main():
         screen.fill(BG)
 
         # Header
-        screen.blit(font_title.render("Horloge moderne (noir/or) — complète", True, (210, 210, 210)), (24, 18))
+        screen.blit(font_title.render("Horloge de mamie Jeannine", True, (210, 210, 210)), (24, 18))
 
         # Cadran
         dial_rect = dial_surf.get_rect(center=(cx, cy))
